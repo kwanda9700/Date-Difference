8467e72b91bc9924afc808b71b8f964aa2a275b5
Kwanda Silekwa <kwanda9700@gmail.com>
2022-04-02 09:20:19 +0200

    Update DateDifference.php
    update

--- DateDifference.php
+++ DateDifference.php
@@ -1,17 +1,21 @@
 <?php
 
 function getDateDifference ($start_date, $end_date) {
+    $start_date = strtotime($start_date);
+    $end_date = strtotime($end_date);
     $datediff = $end_date - $start_date;
-    return $datediff;
+
+    // format timestamp to day[s]
+    $days = (int)str_replace('-','',$datediff  / (24 * 60 * 60)); //24 hours in a day and 60 min in an hour and 60 sec in a min
+
+
+    return $days;
 }
 
-$start_date = strtotime("27 Mar 2022");
-$end_date = strtotime("2022-03-29");
-$date_difference = getDateDifference($start_date, $end_date);
+$start_date = "26 Mar 2022";
+$end_date = "2022-03-29";
 
-// format timestamp to day[s]
-$days = (int)str_replace('-','',$date_difference  / (24 * 60 * 60)); //24 hours in a day and 60 min in an hour and 60 sec in a min
-
+$days = getDateDifference($start_date, $end_date);
 if ($days > 1 )
     print(" Date difference is : " . $days. " days");
 else
